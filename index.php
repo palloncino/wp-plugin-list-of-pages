e5227066f659ceacfe087a3ab77719b880fd88dc
palloncino <antonio.guiotto.dev@gmail.com>
2024-04-01 16:11:16 +0200

    progress

--- index.php
+++ index.php
@@ -3,25 +3,47 @@
 Plugin Name: Amministrazione Trasparente List ID Based Sorting
 Description: Dynamically lists child pages of "Amministrazione Trasparente" into a specific DOM element, sorted by a predefined order of IDs.
 Version: 1.0
-Author: Your Name
+Author: Antonio Guiotto
 */
 
 // Customizable Variables
 $parent_page_id = 581; // ID of the "Amministrazione Trasparente" page
 $target_dom_id = 'amministrazione_trasparente_list'; // ID of the DOM element where the list will be displayed
-$folder_icon_url = 'http://apsproncegno.chebellagiornata.it/wp-content/uploads/2023/12/folder-icon.png'; // URL of the folder icon
+$folder_icon_url = 'http://apsppieve.chebellagiornata.it/wp-content/uploads/2024/03/FOLDER.svg'; // URL of the folder icon
 
 // Predefined order of IDs
 $predefined_order = [
-  700, 874, 950, 976, 1042, 1048, 1080, 1103, 1125, 1139,
-  1142, 1160, 1174, 1222, 1239, 1257, 1283, 1307, 1324,
-  1329, 1333, 1339, 1344,
+    700,  // Disposizioni Generali
+    874,  // Organizzazione
+    950,  // Consulenti e collaboratori
+    976,  // Personale
+    1042, // Bandi di concorso
+    1048, // Performance
+    1080, // Enti controllati
+    1103, // Attività e procedimenti
+    1125, // Provvedimenti
+    1139, // Controlli sulle imprese
+    1142, // Bandi di gara e contratti
+    1160, // Sovvenzioni, contributi, sussidi, vantaggi economici
+    1174, // Bilanci
+    1222, // Beni immobili e gestione patrimonio
+    1239, // Controlli e rilievi sull'amministrazione
+    1257, // Servizi erogati
+    1283, // Pagamenti dell'amministrazione
+    1307, // Opere pubbliche
+    1324, // Pianificazione e governo del territorio
+    1329, // Informazioni ambientali
+    1339, // Interventi straordinari e di emergenza
+    1344, // Altri contenuti
+    1369, // Contatore degli accessi
 ];
 
+
 // Main function to display child pages
-function display_child_pages_list_ordered() {
+function display_child_pages_list_ordered()
+{
     global $parent_page_id, $target_dom_id, $folder_icon_url, $predefined_order;
-    
+
     $args = array(
         'post_type' => 'page',
         'posts_per_page' => -1,
@@ -70,4 +92,3 @@ function display_child_pages_list_ordered() {
 }
 
 add_action('wp_footer', 'display_child_pages_list_ordered');
-?>
